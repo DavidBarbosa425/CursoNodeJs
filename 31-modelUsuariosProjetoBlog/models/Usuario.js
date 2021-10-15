2ca54e745d8893a523505576b1ca8ac9838475c4
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-10-15 15:32:22 -0300

    update

---
+++ 31-modelUsuariosProjetoBlog/models/Usuario.js
@@ -0,0 +1,25 @@
+const Mongoose  = require("mongoose");
+
+const Schema = mongoose.Schema
+
+const Usuario = new Schema({
+
+        nome: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        senha: {
+            type: String,
+            required: true
+        }
+    })
+
+    mongoose.model('usuarios', Usuario)
+
+
+
+
